4b32b0a809a0fb235606b177a8544404046d6916
Can Okay <canokay2014@gmail.com>
2019-04-06 00:23:20 +0300

    init.

---
+++ djangoecomarance_company/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DjangoecomaranceCompanyConfig(AppConfig):
+    name = 'djangoecomarance_company'
---
+++ djangoecomarance_customer/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DjangoecomaranceCustomerConfig(AppConfig):
+    name = 'djangoecomarance_customer'
