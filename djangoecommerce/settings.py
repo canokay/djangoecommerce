b9396f3af0524cab123b9e0fb337242982e6f9ca
Can Okay <canokay2014@gmail.com>
2019-04-06 01:05:27 +0300

    update user model.

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -1,22 +1,9 @@
 import os
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '52s)+f94mcfti(dm87d-d715b10oy_@1wa$o+f4-465qu+plnb'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -57,22 +44,12 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'djangoecommerce.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -87,26 +64,13 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/2.2/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'tr-tr'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = '/static/'
-
 
 AUTH_USER_MODEL = 'djangoecommerce_app.User'
--- djangoecommerce_app/models.py
+++ djangoecommerce_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models import Model,ForeignKey,CharField,DateTimeField,FloatField
+from django.db.models import Model,ForeignKey,CharField,DateTimeField,FloatField,EmailField,ImageField,DateField,TextField, BooleanField
 
 from django.contrib.auth.models import AbstractUser
 
@@ -10,7 +10,18 @@ STATUS=(
 )
 
 class User(AbstractUser):
+    GENDERS = (
+        ('Erkek', 'Erkek'),
+        ('Kadın', 'Kadın'),
+        ('Belirtilmemiş', 'Belirtilmemiş'),
+    )
     user_status = CharField(max_length=15, verbose_name="Kullanıcı Durumu",choices=STATUS)
+    profile_photo = ImageField(verbose_name='Profil Fotoğrafı', upload_to='images/user/profile/', blank=True, null=True)
+    gender = CharField(max_length=100, null=True, blank=True, verbose_name='Cinsiyet', choices=GENDERS)
+    birthday = DateField(null=True, blank=True, verbose_name='Doğum Tarihi')
+    bio = TextField(blank=True, null=True, verbose_name='Biyografi')
+    phone = CharField(max_length=100, null=True, blank=True, verbose_name='Telefon No')
+    is_verified = BooleanField(blank=True, default=True, verbose_name='Onaylandı')
 
     class Meta:
         verbose_name = 'Kullanıcı'
