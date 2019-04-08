2f92f169f3fe0e41df520372e0f46ff2a27a3507
canokay <canokay2014@gmail.com>
2019-04-08 16:03:20 +0300

    old django project deleted

--- djangoecommerce_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.models import Group
-from djangoecommerce_app.models import User
-
-
-admin.site.register(User)
-
-admin.site.unregister(Group)
