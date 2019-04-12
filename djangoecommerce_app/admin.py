9ce7479119ab783ae6680afb85dd7a908837ffa4
Can Okay <canokay2014@gmail.com>
2019-04-12 17:54:35 +0300

    Update admin.py

--- djangoecommerce_app/admin.py
+++ djangoecommerce_app/admin.py
@@ -4,20 +4,33 @@ from django.contrib.auth.models import Group
 from djangoecommerce_app.models import User, City, CompanyAddress, CompanyFeature, Company, ProductCategory,  Product, ProductImage, ProductBrand, ProductStar, Coupon, Card, Order, OrderProductStatus, OrderProductComment
 
 
-admin.site.register(User)
 admin.site.register(City)
 admin.site.register(CompanyAddress)
 admin.site.register(CompanyFeature)
-admin.site.register(Company)
 admin.site.register(ProductCategory)
 admin.site.register(ProductImage)
 admin.site.register(ProductBrand)
 admin.site.register(ProductStar)
 admin.site.register(Coupon)
-admin.site.register(Order)
 admin.site.register(OrderProductStatus)
 admin.site.register(OrderProductComment)
 
+@admin.register(User)
+class UserAdmin(ModelAdmin):
+    list_display = ["id", "username","first_name","last_name","email"]
+    list_display_links = ["id", "username",]
+
+    class Meta:
+        model = User
+
+
+@admin.register(Company)
+class UserAdmin(ModelAdmin):
+    list_display = ["id", "username","first_name","last_name","email"]
+    list_display_links = ["id", "username",]
+
+    class Meta:
+        model = User
 
 
 @admin.register(Product)
@@ -29,6 +42,15 @@ class ProductAdmin(ModelAdmin):
     class Meta:
         model = Product
 
+
+@admin.register(Order)
+class ProductAdmin(ModelAdmin):
+    list_display = ["id", "buyer","owner","status", "coupon", "transaction_id", "transaction_time", "transaction_total_amount"]
+    list_display_links = ["id"]
+    list_editable = ["buyer","owner","status"]
+
+    class Meta:
+        model = Order
 
 
 @admin.register(Card)
