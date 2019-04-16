1833e9777411d20010015401ef73e53f533f1f20
Can Okay <canokay2014@gmail.com>
2019-04-17 01:39:10 +0300

    blog update.

--- djangoecommerce_blog/views.py
+++ djangoecommerce_blog/views.py
@@ -3,23 +3,26 @@ from djangoecommerce_blog.models import BlogCategory
 from djangoecommerce_blog.models import Blog,Hashtag
 
 def IndexView(request):
-    return render(request, 'blog/index.html')
+    context = {
+        "blog": Blog.objects.all()
+    }
+    return render(request, 'blog/index.html', context)
 
 def BlogListView(request):
     context = {
-        "blog": Blog.objects.order_by('-created_date')
+        "blog": Blog.objects.all()
     }
     return render(request, 'blog/blog/blog-list.html', context)
 
 
 def CategoryListView(request):
-    
+
     return render(request, 'blog/category.html')
 
 
 def HashtagListView(request,hashtag):
     hashtag = Hashtag.objects.get(hashtag=hashtag)
-    blog = Blog.objects.filter(hashtag=hashtag).order_by('-created_date')
+    blog = Blog.objects.filter(hashtag=hashtag)
     context = {
         "hashtag": hashtag,
         "blog": blog
@@ -28,8 +31,7 @@ def HashtagListView(request,hashtag):
     return render(request, 'blog/hashtag.html',context)
 
 def BlogDetailView(request,slug):
-    blog = Blog.objects.get(slug=slug)
     context = {
-        "blog": blog
+        "blog": Blog.objects.get(slug=slug)
     }
     return render(request, 'blog/blog-detail.html',context)
